6e379cff92b454d7f6f6e297dfe1901587511c70
田奇 <tianqi05@meituan.com>
2018-06-13 17:43:58 +0800

    update

--- features/feature_extractor.py
+++ features/feature_extractor.py
@@ -18,6 +18,7 @@ parser.add_argument('-s', '--sample', help='use sample data or full data', actio
 parser.add_argument('-f', '--format', help='store pandas feature format, csv, pkl')
 parser.add_argument('-p', '--pool-type', help='pool type, threads or process, here use process for more performance')
 parser.add_argument('-n', '--num-workers', help='workers num in pool')
+parser.add_argument('-b', '--use-pretrained-beta', help='use pre trained beta alpha, beta', action="store_true")
 args = parser.parse_args()
 
 if __name__ == '__main__':
@@ -26,6 +27,7 @@ if __name__ == '__main__':
     fmt = args.format if args.format else 'csv' 
     pool_type = args.pool_type if args.pool_type else 'process'
     n = args.num_workers if args.num_workers else 8
+    use_pretrained = args.use_pretrained_beta
     feature_store_path = '../sample/features' if USE_SAMPLE else '../data/features'
     if not os.path.exists(feature_store_path):
         os.mkdir(feature_store_path)
@@ -34,7 +36,7 @@ if __name__ == '__main__':
     
     feature_to_use = ['exposure_num', 'clicked_num']
     
-    fm_trainer = FeatureMerger(col_feature_store_path, ['exposure_num', 'clicked_num', 'clicked_ratio'], fmt=fmt, data_type='train', pool_type=pool_type, num_workers=n)
+    fm_trainer = FeatureMerger(col_feature_store_path, ['exposure_num', 'clicked_num'], fmt=fmt, data_type='train', pool_type=pool_type, num_workers=n)
     fm_tester = FeatureMerger(col_feature_store_path, ['exposure_num'], fmt=fmt, data_type='test', pool_type=pool_type, num_workers=n)
     
     photo_train = fm_trainer.merge()
@@ -42,37 +44,49 @@ if __name__ == '__main__':
     photo_test = fm_tester.merge()
     print(photo_test.info())
     
-    items = photo_train[['photo_id', 'exposure_num', 'clicked_num', 'clicked_ratio']]
-    items = photo_train.drop_duplicates(['photo_id'])
+    items = photo_train[['photo_id', 'exposure_num', 'clicked_num']]
+    items = items.drop_duplicates(['photo_id'])
     
-    bs = BayesianSmoothing(1, 1)
+    
     I, C = items['exposure_num'].values, items['clicked_num'].values
-    bs.update(I, C, 10000, 0.0000000001)
-    print(bs.alpha, bs.beta)
-    alpha_item, beta_item = bs.alpha, bs.beta
-#     alpha_item, beta_item = 2.8072236088257325, 13.280311727786964
+    if use_pretrained:
+        alpha_item = 2.5171267342473382 if USE_SAMPLE else 2.8072236088257325
+        beta_item = 7.087836849232511 if USE_SAMPLE else 13.280311727786964
+    else:
+        bs = BayesianSmoothing(1, 1)
+        bs.update(I, C, 10000, 0.0000000001)
+        print(bs.alpha, bs.beta)
+        alpha_item, beta_item = bs.alpha, bs.beta
     ctr = []
     for i in range(len(I)):
         ctr.append((C[i]+alpha_item)/(I[i]+alpha_item+beta_item))
     items['clicked_ratio'] = ctr
     items.drop(['exposure_num', 'clicked_num'], axis=1, inplace=True)
-    
+    print(items.head(20))
     clicked_ratio_col_train = pd.merge(photo_train[['user_id', 'photo_id']], items[['photo_id', 'clicked_ratio']],
                                   how='left',
                                   on=['photo_id'])
+    print(clicked_ratio_col_train.head())
+    store_data(clicked_ratio_col_train, os.path.join(col_feature_store_path, 'clicked_ratio_train.csv'), fmt)
     
-
     sigma = items.clicked_ratio.std()
     u = alpha_item/(alpha_item+beta_item)
-    items = photo_test[['photo_id', 'exposure_num']]
-    items = photo_train.drop_duplicates(['photo_id'])
-    items['exposure_num_sigma'] = items['exposure_num'].apply(lambda x: np.exp(-x) * sigma)
+    items1 = photo_test[['photo_id', 'exposure_num']]
+    print(photo_test.head())
+    items1 = items1.drop_duplicates(['photo_id'])
+    items1['exposure_num_sigma'] = items1['exposure_num'].apply(lambda x: np.exp(-x) * sigma)
+    print(items1.head(20))
+    items1['noise'] = items1['exposure_num_sigma'].apply(lambda x: normal(0, x))
+    print(items1.head(20))
+    items1['clicked_ratio'] = u + items1['noise']
+    print(items1.head(20))
+    items1.loc[items1['clicked_ratio']<0, ['clicked_ratio']] = 0
+    print(items1.head(20))
     
-    items['noise'] = items['exposure_num_sigma'].apply(lambda x: normal(0, x))
-    items['clicked_ratio'] = u + items['noise']
-    items.loc[items['clicked_ratio']<0, ['clicked_ratio']] = 0
-    
-    clicked_ratio_col_test = pd.merge(photo_test[['user_id', 'photo_id']], items[['photo_id', 'clicked_ratio']],
+    clicked_ratio_col_test = pd.merge(photo_test[['user_id', 'photo_id']], items1[['photo_id', 'clicked_ratio']],
                                   how='left',
                                   on=['photo_id'])
+    print(clicked_ratio_col_test.head())
+
+    store_data(clicked_ratio_col_test, os.path.join(col_feature_store_path, 'clicked_ratio_test.csv'), fmt)
     
