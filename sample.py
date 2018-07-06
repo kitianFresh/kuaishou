514fdaa6b32aea159e5b6b39e6012753afff8fb4
coffee <yafeikou@gmail.com>
2018-07-06 11:16:08 +0800

    修改visual特征路径名

--- sample.py
+++ sample.py
@@ -80,11 +80,9 @@ sample_text_train = photo_sample(text_train, sample_photo_ids_train)
 sample_text_train.to_csv('./sample/train_text.txt', sep='\t', index=False, header=False)
 print('text_train sampled')
 
-visual_train_path = './data/preliminary_visual_train'
-visual_test_path = './data/preliminary_visual_test'
 
-sample_visual_train_path = './sample/preliminary_visual_train'
-sample_visual_test_path = './sample/preliminary_visual_test'
+sample_visual_train_path = './sample/visual_train'
+sample_visual_test_path = './sample/visual_test'
 
 sample_visual_train = open(sample_visual_train_path,'w')
 for photo_id in sample_photo_ids_train:
