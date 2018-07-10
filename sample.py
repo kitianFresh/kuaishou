24f04ece98ec0fbc0b18a5d85656ae21deed7d5f
田奇 <tianqi05@meituan.com>
2018-07-10 16:16:38 +0800

    fix float and int

--- sample.py
+++ sample.py
@@ -14,7 +14,7 @@ parser.add_argument('-p', '--prop', help='sample propotion, default 0.15', defau
 args = parser.parse_args()
 
 
-random.seed(a=args.seed)
+random.seed(a=int(args.seed))
 
 def user_sample(group, prop):
     n = group.shape[0]
@@ -57,13 +57,13 @@ text_test = pd.read_csv('./data/test_text.txt',
                        header=None,
                        names=['photo_id', 'cover_words'])
 
-user_sample = functools.partial(user_sample, prop=args.prop)
+user_sample = functools.partial(user_sample, prop=float(args.prop))
 sample_user_item_test = user_item_test.groupby(['user_id']).apply(user_sample)
 sample_user_item_test.reset_index(drop=True, inplace=True)
 sample_user_item_test.to_csv('./sample/test_interaction.txt', sep='\t', index=False, header=False)
 print('user_item_test sampled')
 
-user_sample = functools.partial(user_sample, prop=args.prop)
+user_sample = functools.partial(user_sample, prop=float(args.prop))
 sample_user_item_train = user_item_train.groupby(['user_id']).apply(user_sample)
 sample_user_item_train.reset_index(drop=True, inplace=True)
 sample_user_item_train.to_csv('./sample/train_interaction.txt', sep='\t', index=False, header=False)
