d561827431899ee1b864ab551fe97b19f8f91435
田奇 <tianqi05@meituan.com>
2018-07-14 15:45:10 +0800

    update catbst offline

--- models/offline/catbst.py
+++ models/offline/catbst.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     down_sample_rate = float(args.down_sampling)
     k = int(args.topk_features)
     num_workers = args.num_workers
-    
+    kfold = 0
     model_name = 'catboost'
 
     model_store_path = './sample/' if USE_SAMPLE else './data'
@@ -60,17 +60,15 @@ if __name__ == '__main__':
 
     start = time.time()
     if all_one:
-        ALL_FEATURE_TRAIN_FILE = 'ensemble_feature_train'
-        ALL_FEATURE_TRAIN_FILE = ALL_FEATURE_TRAIN_FILE + '_sample' + '.' + fmt if USE_SAMPLE else ALL_FEATURE_TRAIN_FILE + '.' + fmt
-        ensemble_train = read_data(os.path.join(feature_store_path, ALL_FEATURE_TRAIN_FILE), fmt)
+        ALL_FEATURE_TRAIN_FILE = 'ensemble_feature_train' + str(kfold) + '.' + fmt
+        ensemble_train = read_data(os.path.join(feature_store_dir, ALL_FEATURE_TRAIN_FILE), fmt)
 
-        ALL_FEATURE_TEST_FILE = 'ensemble_feature_test'
-        ALL_FEATURE_TEST_FILE = ALL_FEATURE_TEST_FILE + '_sample' + '.' + fmt if USE_SAMPLE else ALL_FEATURE_TEST_FILE + '.' + fmt
-        ensemble_test = read_data(os.path.join(feature_store_path, ALL_FEATURE_TEST_FILE), fmt)
+        ALL_FEATURE_TEST_FILE = 'ensemble_feature_test' + str(kfold) + '.' + fmt
+        ensemble_test = read_data(os.path.join(feature_store_dir, ALL_FEATURE_TEST_FILE), fmt)
     else:
         feature_to_use = user_features + photo_features + time_features
-        fm_trainer = FeatureMerger(col_feature_store_path, feature_to_use+y_label, fmt=fmt, data_type='train', pool_type='process', num_workers=num_workers)
-        fm_tester = FeatureMerger(col_feature_store_path, feature_to_use, fmt=fmt, data_type='test', pool_type='process', num_workers=num_workers)
+        fm_trainer = FeatureMerger(col_feature_store_dir, feature_to_use+y_label, fmt=fmt, data_type='train', pool_type='process', num_workers=num_workers)
+        fm_tester = FeatureMerger(col_feature_store_dir, feature_to_use, fmt=fmt, data_type='test', pool_type='process', num_workers=num_workers)
         ensemble_train = fm_trainer.merge()
         ensemble_test = fm_tester.merge()
 
@@ -100,23 +98,12 @@ if __name__ == '__main__':
         print(ensemble_train_neg.shape)
         ensemble_train = pd.concat([ensemble_train_pos, ensemble_train_neg])
 
-    # 决策树模型不需要归一化，本身就是范围划分
-
     print('Training model %s......' % model_name)
 
-    ensemble_train = ensemble_train.sort_values('time')
-    train_num = ensemble_train.shape[0]
-    train_data = ensemble_train.iloc[:int(train_num * 0.7)].copy()
-    val_data = ensemble_train.iloc[int(train_num * 0.7):].copy()
+    X_train, y_train = ensemble_train[features_to_train].values, ensemble_train[y_label].values
 
-    print(train_data.shape)
-    print(val_data.shape)
-    val_photo_ids = list(set(val_data['photo_id'].unique()) - set(train_data['photo_id'].unique()))
-    val_data = val_data.loc[val_data.photo_id.isin(val_photo_ids)]
-    print(val_data.shape)
-    X_train, X_val, y_train, y_val = train_data[features_to_train].values, val_data[features_to_train].values, \
-                                     train_data[y_label].values, val_data[y_label].values
-
+    X_val, y_val = ensemble_test[features_to_train].values, ensemble_test[y_label].values
+    
     print(y_train.mean(), y_train.std())
     print(y_val.mean(), y_val.std())
 
@@ -131,13 +118,7 @@ if __name__ == '__main__':
     start = time.time()
     model.clf = CatBoostClassifier(task_type='GPU' if gpu_mode else 'CPU', gpu_cat_features_storage='CpuPinnedMemory', pinned_memory_size=2147483648, logging_level='Debug')
     model.clf.fit(X_train, y_train.ravel(), cat_features=cat_feature_inds, eval_set=(X_train, y_train.ravel()))
-    # # KFold cross validation
-    # def cross_validate(*args, **kwargs):
-    #     cv = StratifiedKFold(n_splits=3, random_state=0, shuffle=False)
-    #     scores = cross_val_score(model.clf, X, y.ravel(), cv=cv, scoring='roc_auc')
-    #     return scores
-    #
-    # model.cross_validation(cross_validate)
+    
     print("Model trained in %s seconds" % (str(time.time() - start)))
     if down_sample_rate < 1.:
         model.compute_metrics(X_val, y_val.ravel(), calibration_weight=down_sample_rate)
@@ -168,9 +149,5 @@ if __name__ == '__main__':
 
         model.compute_features_distribution(golden_features)
 
-    if down_sample_rate < 1.:
-        model.submit(ensemble_test, calibration_weight=down_sample_rate)
-    else:
-        model.submit(ensemble_test)
     model.save()
 
