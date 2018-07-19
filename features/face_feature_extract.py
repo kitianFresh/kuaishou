38447259eff7ad8602d0330a53b378363b328a20
田奇 <tianqi05@meituan.com>
2018-07-19 11:35:33 +0800

    kfold default =0

--- features/face_feature_extract.py
+++ features/face_feature_extract.py
@@ -32,7 +32,7 @@ def add_face_feature(face_data):
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--format', help='store pandas feature format, csv, pkl')
 parser.add_argument('-o', '--online', help='online feature extract', action="store_true")
-parser.add_argument('-k', '--offline-kfold', help='offline kth fold feature extract, extract kth fold', default=1)
+parser.add_argument('-k', '--offline-kfold', help='offline kth fold feature extract, extract kth fold', default=0)
 args = parser.parse_args()
 
 if __name__ == '__main__':
