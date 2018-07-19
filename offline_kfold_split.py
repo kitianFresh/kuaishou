b474240287ec9a527026eae946b4004ed193ae54
田奇 <tianqi05@meituan.com>
2018-07-19 11:45:23 +0800

    add some print

--- offline_kfold_split.py
+++ offline_kfold_split.py
@@ -1,3 +1,5 @@
+#coding:utf8
+
 import random
 import pandas as pd
 import functools
@@ -77,66 +79,98 @@ for i in range(kfold):
     user_sample = functools.partial(user_sample, prop=prop)
     kfold_user_item_val = user_item_train.groupby(['user_id']).apply(user_sample)
     kfold_user_item_val.reset_index(drop=True, inplace=True)
-    kfold_users = kfold_user_item_val['user_id'].unique()
-    kfold_user_item_train = user_item_train.append(kfold_user_item_val).drop_duplicates(keep=False).reset_index(drop=True)
-    val_photo_ids = list(set(kfold_user_item_val['photo_id'].unique()) - set(kfold_user_item_train['photo_id'].unique()))
-    print(len(kfold_users))
-    print(len(val_photo_ids))
-    print(len(set(kfold_user_item_val['photo_id'].unique())))
+
+    kfold_user_item_train = user_item_train.append(kfold_user_item_val).drop_duplicates(keep=False).reset_index(
+        drop=True)
+    val_photo_ids = set(kfold_user_item_val['photo_id'].unique()) - set(kfold_user_item_train['photo_id'].unique())
+    inter_train_val_photo_ids = set(kfold_user_item_val['photo_id'].unique()) & set(
+        kfold_user_item_train['photo_id'].unique())
+    # online: train_user_ids = test_user_ids; train_photo_ids & test_photo_ids = null; train_time.max < test_time.min;
+    # offline: this train will include train val intersection inter_train_val_photo_ids;
+    # satisfy: train_user_ids = val_user_ids; train_photo_ids & val_photo_ids = null; train_time.max < val_time.min;
+    train_photo_ids = kfold_user_item_train['photo_id'].unique()
+    print("train shape: (%d, %d)" % (kfold_user_item_train.shape[0], kfold_user_item_train.shape[1]))
+    print("valid shape: (%d, %d)" % (kfold_user_item_val.shape[0], kfold_user_item_val.shape[1]))
+    kfold_val_users = kfold_user_item_val['user_id'].unique()
+    kfold_train_users = kfold_user_item_train['user_id'].unique()
+
+    print("val users: %s" % len(kfold_val_users))
+    print("train users: %s" % len(kfold_train_users))
+
+    print("valid photos before remove intersection: %s" % len(set(kfold_user_item_val['photo_id'].unique())))
+    print("valid photos after remove train/val intersection: %s" % len(val_photo_ids))
     print('train click mean: %s' % user_item_train['click'].mean())
-    kfolds.append((kfold_users, set(val_photo_ids)))
     print('val click mean before remove intersection: %s' % kfold_user_item_val['click'].mean())
     kfold_user_item_val = kfold_user_item_val.loc[kfold_user_item_val.photo_id.isin(val_photo_ids)]
     print('val click mean after remove intersection: %s' % kfold_user_item_val['click'].mean())
-    pos_kfold_user_item_val = kfold_user_item_val[kfold_user_item_val['click']==1]
-    neg_kfold_user_item_val =  kfold_user_item_val[kfold_user_item_val['click']==0]
-    # negative_sample_ratio = (1-ctr_o)*c_s/(ctr_o * u_s)
-    ctr_o = user_item_train['click'].mean()
-    c_s = pos_kfold_user_item_val.shape[0]
-    u_s = neg_kfold_user_item_val.shape[0]
-    print(ctr_o, c_s, u_s)
-    negative_sample_ratio = (1 - ctr_o) * c_s / (ctr_o * u_s)
-    print(negative_sample_ratio)
-    def negative_sample(group):
-        n = group.shape[0]
-        m = int(negative_sample_ratio * n)
-        m = n if m == 0 else m
-        group = group.iloc[random.sample(range(n), m)]
-        return group
-    neg_kfold_user_item_val = neg_kfold_user_item_val.groupby(['user_id']).apply(negative_sample)
-    kfold_user_item_val = pd.concat([pos_kfold_user_item_val, neg_kfold_user_item_val]).reset_index(drop=True)
-    val_photo_ids = set(kfold_user_item_val['photo_id'].unique())
-    kfold_user_item_train = user_item_train.loc[~user_item_train.photo_id.isin(val_photo_ids)]
-    print('val click mean after neg sample: %s' % kfold_user_item_val['click'].mean())
-    print('train click mean after neg sample: %s' % kfold_user_item_train['click'].mean())
+    print("train shape after validation remove intersection: (%d, %d)" % (
+    kfold_user_item_train.shape[0], kfold_user_item_train.shape[1]))
+    print("valid shape after validation remove intersection: (%d, %d)" % (
+    kfold_user_item_val.shape[0], kfold_user_item_val.shape[1]))
 
-                        
-    kfold_user_item_val.to_csv(os.path.join(offline_data_dir, 'test_interaction' + str(i) + '.txt'), sep='\t', index=False, header=False)
+    # pos_kfold_user_item_val = kfold_user_item_val[kfold_user_item_val['click']==1]
+    # neg_kfold_user_item_val =  kfold_user_item_val[kfold_user_item_val['click']==0]
+    # # negative_sample_ratio = (1-ctr_o)*c_s/(ctr_o * u_s)
+    # ctr_o = user_item_train['click'].mean()
+    # c_s = pos_kfold_user_item_val.shape[0]
+    # u_s = neg_kfold_user_item_val.shape[0]
+    # print(ctr_o, c_s, u_s)
+    # negative_sample_ratio = (1 - ctr_o) * c_s / (ctr_o * u_s)
+    # print(negative_sample_ratio)
+    # def negative_sample(group):
+    #     # make sure validation time took after train
+    #     group = group.sort_values('time', ascending=False)
+    #     n = group.shape[0]
+    #     m = int(negative_sample_ratio * n)
+    #     m = n if m == 0 else m
+    #     group = group.iloc[random.sample(range(n), m)]
+    #     return group
+    # neg_kfold_user_item_val = neg_kfold_user_item_val.groupby(['user_id']).apply(negative_sample)
+    # kfold_user_item_val = pd.concat([pos_kfold_user_item_val, neg_kfold_user_item_val]).reset_index(drop=True)
+    # val_photo_ids = set(kfold_user_item_val['photo_id'].unique())
+    # print('val click mean after neg sample: %s' % kfold_user_item_val['click'].mean())
+    # print('train click mean after neg sample: %s' % kfold_user_item_train['click'].mean())
+    # print("train shape after validation neg sample: (%d, %d)" % (kfold_user_item_train.shape[0], kfold_user_item_train.shape[1]))
+    # print("valid shape after validation neg sample: (%d, %d)" % (kfold_user_item_val.shape[0], kfold_user_item_val.shape[1]))
+
+    user_item_val_path = os.path.join(offline_data_dir, 'test_interaction' + str(i) + '.txt')
+    kfold_user_item_val.to_csv(user_item_val_path, sep='\t', index=False, header=False)
     print(kfold_user_item_val.info())
-    print('The %dth fold test_interaction extracted' % i)
-    kfold_user_item_train.to_csv(os.path.join(offline_data_dir, 'train_interaction' + str(i) + '.txt'), sep='\t', index=False, header=False)
+    print('The %dth fold test_interaction extracted to %s' % (i, user_item_val_path))
+    user_item_train_path = os.path.join(offline_data_dir, 'train_interaction' + str(i) + '.txt')
+    kfold_user_item_train.to_csv(user_item_train_path, sep='\t', index=False, header=False)
     print(kfold_user_item_train.info())
-    print('The %dth fold train_interaction extracted' % i)
+    print('The %dth fold train_interaction extracted to %s' % (i, user_item_train_path))
 
-    train_photo_ids = set(kfold_user_item_train['photo_id'].unique()) - set(val_photo_ids)
+    # 这里取train_photo_ids 的话，就是训练集会加入更多的来自未做交集之前的验证集合的样例，但是无法保证时间完全先后顺序
+    # train_photo_ids = set(kfold_user_item_train['photo_id'].unique()) - val_photo_ids
 
     kfold_face_val = photo_sample(face_train, val_photo_ids)
     kfold_face_train = photo_sample(face_train, train_photo_ids)
-    kfold_face_val.to_csv(os.path.join(offline_data_dir, 'test_face' + str(i) + '.txt'), sep='\t', index=False, header=False)
+    face_test_path = os.path.join(offline_data_dir, 'test_face' + str(i) + '.txt')
+    kfold_face_val.to_csv(face_test_path, sep='\t', index=False, header=False)
     print(kfold_face_val.info())
-    print('The %dth fold test_face extracted' % i)
-    kfold_face_train.to_csv(os.path.join(offline_data_dir, 'train_face' + str(i) + '.txt'), sep='\t', index=False, header=False)
+    print('The %dth fold test_face extracted to %s' % (i, face_test_path))
+    face_train_path = os.path.join(offline_data_dir, 'train_face' + str(i) + '.txt')
+    kfold_face_train.to_csv(face_train_path, sep='\t', index=False, header=False)
     print(kfold_face_train.info())
-    print('The %dth fold train_face extracted' % i)
+    print('The %dth fold train_face extracted to %s' % (i, face_train_path))
 
     kfold_text_val = photo_sample(text_train, val_photo_ids)
     kfold_text_train = photo_sample(text_train, train_photo_ids)
-    kfold_text_val.to_csv(os.path.join(offline_data_dir, 'test_text' + str(i) + '.txt'), sep='\t', index=False, header=False)
+    text_test_path = os.path.join(offline_data_dir, 'test_text' + str(i) + '.txt')
+    kfold_text_val.to_csv(text_test_path, sep='\t', index=False, header=False)
     print(kfold_text_val.info())
-    print('The %dth fold test_text extracted' % i)
-    kfold_text_train.to_csv(os.path.join(offline_data_dir, 'train_text' + str(i) + '.txt'), sep='\t', index=False, header=False)
+    print('The %dth fold test_text extracted to %s' % (i, text_test_path))
+    text_train_path = os.path.join(offline_data_dir, 'train_text' + str(i) + '.txt')
+    kfold_text_train.to_csv(text_train_path, sep='\t', index=False, header=False)
     print(kfold_text_train.info())
-    print('The %dth fold train_text extracted' % i)
+    print('The %dth fold train_text extracted to %s' % (i, text_train_path))
+
+    train_text_photos = set(kfold_user_item_train['photo_id'].unique())
+    ui_photos = set(kfold_text_train['photo_id'].unique())
+    print(len(train_text_photos), len(ui_photos))
+    print(len(train_text_photos & ui_photos))
 
 import matplotlib.pyplot as plt
 from matplotlib_venn import venn3
