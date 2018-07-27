aaad4f5fac0f33dbc017fe3f5050198da99c9daf
田奇 <tianqi05@meituan.com>
2018-07-27 12:15:41 +0800

    update visual feature extract, use only train to train cluster model

--- features/visual_feature_extract.py
+++ features/visual_feature_extract.py
@@ -20,30 +20,30 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--sample', help='use sample data or full data', action="store_true")
 parser.add_argument('-f', '--format', help='store pandas feature format, csv, pkl')
 parser.add_argument('-o', '--online', help='use online data', action='store_true')
+parser.add_argument('-k', '--offline-kfold', help='offline kth fold feature extract, extract kth fold', default=0)
 args = parser.parse_args()
 
 
 if __name__ == '__main__':
     USE_SAMPLE = args.sample
     fmt = args.format if args.format else 'csv'
-
+    kfold = int(args.offline_kfold)
     if args.online:
-        data_file_pre = 'online/'
+        TRAIN_USER_INTERACT, online_data_dir, feature_store_dir, col_feature_store_dir = get_data_file(
+            'train_interaction.txt')
+        TEST_USER_INTERACT, online_data_dir, feature_store_dir, col_feature_store_dir = get_data_file(
+            'test_interaction.txt')
     else:
-        data_file_pre = 'offline/'
+        TRAIN_USER_INTERACT, offline_data_dir, feature_store_dir, col_feature_store_dir = get_data_file(
+            'train_interaction' + str(kfold) + '.txt', online=False)
+        TEST_USER_INTERACT, offline_data_dir, feature_store_dir, col_feature_store_dir = get_data_file(
+            'test_interaction' + str(kfold) + '.txt', online=False)
 
-    TRAIN_USER_INTERACT = '../sample/'+data_file_pre+'train_interaction.txt' if USE_SAMPLE else '../data/'+data_file_pre+'train_interaction.txt'
-    TEST_USER_INTERACT = '../sample/'+data_file_pre+'test_interaction.txt' if USE_SAMPLE else '../data/'+data_file_pre+'test_interaction.txt'
-
-    TRAIN_VISUAL_DIR = '../sample/'+data_file_pre+'visual_train' if USE_SAMPLE else '../data/'+data_file_pre+'final_visual_train.zip'
-    TEST_VISUAL_DIR = '../sample/'+data_file_pre+'visual_test' if USE_SAMPLE else '../data/'+data_file_pre+'final_visual_test.zip'
-
-    TRAIN_VISUAL_MATRIX = '../sample/'+data_file_pre+'visual_train_matrix.blp' if USE_SAMPLE else '../data/'+data_file_pre+'visual_train_matrix.blp'
-    TEST_VISUAL_MATRIX = '../sample/'+data_file_pre+'visual_test_matrix.blp' if USE_SAMPLE else '../data/'+data_file_pre+'visual_test_matrix.blp'
-
-
-    TRAIN_VISUAL_PHOTO_ID = '../sample/'+data_file_pre+'visual_train_photo_id.blp' if USE_SAMPLE else '../data/'+data_file_pre+'visual_train_photo_id.blp'
-    TEST_VISUAL_PHOTO_ID = '../sample/'+data_file_pre+'visual_test_photo_id.blp' if USE_SAMPLE else '../data/'+data_file_pre+'visual_test_photo_id.blp'
+    TRAIN_VISUAL_MATRIX = os.path.join(online_data_dir, 'visual_train_matrix.blp')
+    TEST_VISUAL_MATRIX = os.path.join(online_data_dir, 'visual_test_matrix.blp')
+    TRAIN_VISUAL_PHOTO_ID = os.path.join(online_data_dir, 'visual_train_photo_id.blp')
+    TEST_VISUAL_PHOTO_ID = os.path.join(online_data_dir, 'visual_test_photo_id.blp')
+    VISUAL_CLUSTER_MODEL = os.path.join(online_data_dir, 'visual_cluster_model_kmeans.pkl')
 
     user_item_train = pd.read_csv(TRAIN_USER_INTERACT,
                                   sep='\t',
@@ -56,8 +56,6 @@ if __name__ == '__main__':
                                   header=None,
                                   usecols =[0,1],
                                   names=['user_id', 'photo_id'])
-
-    user_item_data = pd.concat([user_item_train,user_item_test])
 
 
     def concat(X, Y, copy=False):
@@ -72,42 +70,42 @@ if __name__ == '__main__':
             ret[len_x + j] = Y[j]
         return ret
 
-    matrix = None
-    photo_id = None
     if os.path.exists(TRAIN_VISUAL_MATRIX) and os.path.exists(TEST_VISUAL_MATRIX):
         train_matrix,train_photo_id = load_data(TRAIN_VISUAL_MATRIX,TRAIN_VISUAL_PHOTO_ID)
         test_matrix,test_photo_id = load_data(TEST_VISUAL_MATRIX,TEST_VISUAL_PHOTO_ID)
-        matrix = concat(train_matrix,test_matrix)
-        photo_id = concat(train_photo_id,test_photo_id)
     else:
-        if USE_SAMPLE:
-            sample_data_transform_store(TRAIN_VISUAL_DIR,TRAIN_VISUAL_MATRIX,TRAIN_VISUAL_PHOTO_ID)
-            sample_data_transform_store(TEST_VISUAL_DIR,TEST_VISUAL_MATRIX,TEST_VISUAL_PHOTO_ID)
-            train_matrix,train_photo_id = load_data(TRAIN_VISUAL_MATRIX,TRAIN_VISUAL_PHOTO_ID)
-            test_matrix, test_photo_id = load_data(TEST_VISUAL_MATRIX, TEST_VISUAL_PHOTO_ID)
-            matrix = np.vstack([train_matrix, test_matrix])
-            photo_id = np.append(train_photo_id, test_photo_id)
-        else:
-            data_transform_store(TRAIN_VISUAL_DIR, TRAIN_VISUAL_MATRIX, TRAIN_VISUAL_PHOTO_ID)
-            data_transform_store(TEST_VISUAL_DIR, TEST_VISUAL_MATRIX, TEST_VISUAL_PHOTO_ID)
-            train_matrix, train_photo_id = load_data(TRAIN_VISUAL_MATRIX, TRAIN_VISUAL_PHOTO_ID)
-            test_matrix, test_photo_id = load_data(TEST_VISUAL_MATRIX, TEST_VISUAL_PHOTO_ID)
-            matrix = np.vstack([train_matrix, test_matrix])
-            photo_id = np.append(train_photo_id, test_photo_id)
+        raise IOError('No matrix')
 
-    VISUAL_CLUSTER_MODEL = '../sample/'+data_file_pre+'visual_cluster_model' if USE_SAMPLE else '../data/'+data_file_pre+'visual_cluster_model'
     cluster_nums = 50
-    visual_feature = None
-    if os.path.exists(VISUAL_CLUSTER_MODEL + '_kmeans.pkl'):
-        visual_feature = cluster_model_predict(VISUAL_CLUSTER_MODEL,matrix,photo_id)
+    if os.path.exists(VISUAL_CLUSTER_MODEL):
+        visual_feature_train = cluster_model_predict(VISUAL_CLUSTER_MODEL,train_matrix,train_photo_id)
+        visual_feature_test = cluster_model_predict(VISUAL_CLUSTER_MODEL, test_matrix, test_photo_id)
     else:
-        visual_feature = train_cluster_model(matrix,photo_id,VISUAL_CLUSTER_MODEL,cluster_nums)
+        visual_feature_train = train_cluster_model(train_matrix,train_photo_id,VISUAL_CLUSTER_MODEL,cluster_nums)
+        visual_feature_test = cluster_model_predict(VISUAL_CLUSTER_MODEL, test_matrix, test_photo_id)
 
-    visual_feature = pd.merge(user_item_data,visual_feature,how='left',on=['photo_id'])
-    visual_feature['photo_cluster_label'] = visual_feature['photo_cluster_label'].astype(feature_dtype_map['photo_cluster_label'])
-    feature_store_path = '../sample/'+data_file_pre+'features' if USE_SAMPLE else '../data/'+data_file_pre+'features'
-    if not os.path.exists(feature_store_path):
-        os.mkdir(feature_store_path)
-    VISUAL_FEATURE_FILE = 'visual_feature'
-    VISUAL_FEATURE_FILE = VISUAL_FEATURE_FILE + '_sample' + '.' + fmt if USE_SAMPLE else VISUAL_FEATURE_FILE + '.' + fmt
-    store_data(visual_feature, os.path.join(feature_store_path, VISUAL_FEATURE_FILE), fmt)
+    visual_feature_train = pd.merge(user_item_train,visual_feature_train,how='left',on=['photo_id'])
+    visual_feature_test = pd.merge(user_item_test,visual_feature_test,how='left',on=['photo_id'])
+
+    visual_feature_train['photo_cluster_label'] = visual_feature_train['photo_cluster_label'].astype(feature_dtype_map['photo_cluster_label'])
+    visual_feature_test['photo_cluster_label'] = visual_feature_test['photo_cluster_label'].astype(feature_dtype_map['photo_cluster_label'])
+
+    visual_feature_train.sort_values(['user_id', 'photo_id'], inplace=True)
+    visual_feature_test.sort_values(['user_id', 'photo_id'], inplace=True)
+
+    print(visual_feature_train.info())
+    print(visual_feature_test.info())
+    print(np.sum(visual_feature_train.isnull()))
+    print(np.sum(visual_feature_test.isnull()))
+
+    VISUAL_FEATURE_TRAIN_FILE = 'visual_feature_train' + '.' + fmt
+    VISUAL_FEATURE_TEST_FILE = 'visual_feature_test' + '.' + fmt
+
+    store_data(visual_feature_train, os.path.join(feature_store_dir, VISUAL_FEATURE_TRAIN_FILE), fmt)
+    store_data(visual_feature_test, os.path.join(feature_store_dir, VISUAL_FEATURE_TEST_FILE), fmt)
+
+    # column
+    for col in set(visual_feature_train.columns) - set(['user_id', 'photo_id']):
+        store_data(visual_feature_train[[col]], os.path.join(col_feature_store_dir, col + '_train.csv'), fmt)
+        store_data(visual_feature_test[[col]], os.path.join(col_feature_store_dir, col + '_test.csv'), fmt)
+
