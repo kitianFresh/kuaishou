5e7d3ed16c73bdd8ac15e97d4fb47cd451643113
田奇 <tianqi05@meituan.com>
2018-07-31 09:15:50 +0800

    iterations=10000

--- models/offline/lgbm.py
+++ models/offline/lgbm.py
@@ -117,7 +117,7 @@ if __name__ == '__main__':
     start_time_1 = time.time()
 
     model.clf = LGBMClassifier(boosting_type='gbdt', num_leaves=63, reg_alpha=0.0, reg_lambda=1,
-                                max_depth=-1, n_estimators=1200, objective='binary',
+                                max_depth=-1, n_estimators=10000, objective='binary',
                                 subsample=0.8, colsample_bytree=0.8, subsample_freq=1, feature_fraction=0.8,
                                 learning_rate=0.05, min_child_weight=200,
                                 random_state=2018, n_jobs=-1, device='gpu' if args.gpu_mode else 'cpu',
